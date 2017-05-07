01f5cbfa711b1bc70cd7a6958289b0e350292622
Akira Laine <akira_laine@outlook.com>
2017-05-07 16:20:32 +1100

    initial work on room page

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>room</div>
+  <div class='wrapper'>
+    <div class='column-2'>
+      <div class='top-section'>
+        <h4 class='logo'>View</h4>
+        <span class='link'>https://view.now.sh/room/123456</span>
+      </div>
+    </div>
+    <div class='column-1'>
+      
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +17,40 @@
     name: 'room'
   }
 </script>
+
+<style>
+.wrapper {
+  background-color: #f6f6f6;
+  width: 100vw;
+  height: 100vh;
+  padding: 2em;
+  display: flex;
+}
+.column-1 {
+  width: 33%;
+  height: 100%;
+  /*background-color: #666;*/
+  padding: 10px;
+}
+.column-2 {
+  width: 66%;
+  height: 100%;
+  /*background-color: #ccc;*/
+  padding: 10px;
+}
+.top-section {
+  width: 100%;
+  display: flex;
+  align-items: baseline;
+}
+.logo {
+  font-weight: 300;
+  font-size: 2em;
+  letter-spacing: 2px;
+  color: #E35D5B;
+}
+.link {
+  margin-left: 5px;
+  color: #E35D5B;
+}
+</style>
