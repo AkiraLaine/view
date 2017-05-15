04c4ca9dd3edebd30046f77ef65105895e91bba7
Akira Laine <akira_laine@outlook.com>
2017-05-15 12:42:14 +1100

    removed horizon from main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
-import Horizon from 'vue-horizon'
 
 import App from './App'
 import router from './router'
 
-const URL = process.env.NODE_ENV === 'production' ? '104.131.108.12' : 'localhost'
-
 Vue.config.productionTip = false
-Vue.use(Horizon, {
-  host: `${URL}:8181`,
-  models: ['rooms']
-})
 
 /* eslint-disable no-new */
 new Vue({
